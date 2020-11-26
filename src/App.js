e5adb2f61a55c3ecc79f5aba142b3038290b0ee3
Vageshwar <vageshwaryadav@gmail.com>
2020-11-26 10:58:27 +0530

    "Done"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback, useEffect} from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World!</h1>
+//     </div>
+//   );
+// }
+
+const App = () => {
+
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const onNewTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value);
+    console.log(event.target.value);
+  }, []); 
+
+  const formSubmitted = useCallback((event) => {
+    console.log("Form Submitted");
+    setTodos(
+      [...todos , {
+        id: todos.length + 1,
+        content : newTodo,
+        done: false
+      }]
+    );
+    setNewTodo("");
+    event.preventDefault();
+  }, [newTodo, todos] );
+
+  useEffect( () => {
+    console.log('todos', todos);
+  }, [todos]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <form onSubmit= {formSubmitted}>
+        <lable htmlFor="todo"> Enter Data </lable>
+        <input id="todo" name="todo" value={newTodo}  onChange = {onNewTodoChange}/>
+
+        <button> Add Todo</button>
+      </form>
+      <h1>My Todo List</h1>
+      <ul>
+        {
+          todos.map((e) => (
+            
+            <li  key={e.id}> <input type="checkbox" /> {e.content} </li>
+          ))
+        }
+      </ul>
+  </div>
   );
 }
 
